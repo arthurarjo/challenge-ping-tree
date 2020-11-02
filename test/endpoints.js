808b1adfb75d7f93ab27cf6d7b4a8fec1b7dbc95
Arthur Araujo <aaraujjo@outlook.com.br>
2020-11-02 17:36:20 -0300

    create get all route and test

--- test/endpoints.js
+++ test/endpoints.js
@@ -4,6 +4,15 @@ var test = require('ava')
 var servertest = require('servertest')
 
 var server = require('../lib/server')
+var targetsModel = require('../lib/models/targets')
+
+var defaultTarget = {
+  id: 'targets:1',
+  url: 'http://example.com',
+  value: '0.50',
+  maxAcceptsPerDay: '10',
+  accept: 'true'
+}
 
 test.serial.cb('healthcheck', function (t) {
   var url = '/health'
@@ -13,5 +22,18 @@ test.serial.cb('healthcheck', function (t) {
     t.is(res.statusCode, 200, 'correct statusCode')
     t.is(res.body.status, 'OK', 'status is ok')
     t.end()
+  })
+})
+
+test.serial.cb('shoud get all targets', function (t) {
+  var url = '/api/targets'
+  targetsModel.addSet(defaultTarget, function (err, targets) {
+    t.falsy(err, 'no-model-error')
+    servertest(server(), url, { encoding: 'json' }, function (err, res) {
+      t.falsy(err, 'no error')
+      t.is(res.statusCode, 200, 'correct statusCode')
+      t.deepEqual(res.body, [defaultTarget], 'correct body content')
+      t.end()
+    })
   })
 })
